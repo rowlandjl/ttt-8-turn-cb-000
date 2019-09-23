c61aa4a6a2b639bb2b6251ce56621eeb83be9542
Jessica Rowland <rowlandjl@vcu.edu>
2019-09-23 12:50:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,6 +12,10 @@ end
 
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index].nil?
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end 
 end
 
 def move
