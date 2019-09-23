f65006fb779c008f99c28d2d8cac71ca2c56b617
Jessica Rowland <rowlandjl@vcu.edu>
2019-09-23 13:15:05 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,7 +34,7 @@ def turn(board)
   while !valid_move?(board, index)
     puts "Please enter 1-9:"
     input = gets.strip
-    index = input_to_index
+    index = input_to_index(input)
   end
 
 end
